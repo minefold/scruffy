91386d81cc1a52288e687d3d65754c9d5915a88f
Dave Newman <dave@snappyco.de>
2012-11-06 23:50:05 -0800

    initial commit

--- test/scruffy_test.rb
+++ test/scruffy_test.rb
@@ -16,49 +16,6 @@ class Scruffy
   end
 end
 
-describe BoxType do
-  let(:box_type) { BoxType.find('c1.xlarge') }
-  it "has attributes" do
-    box_type.id.must_equal 'c1.xlarge'
-    box_type.ram.must_equal 7.0 * 1024
-    box_type.ecus.must_equal 20
-    box_type.ami.must_equal BoxType::AMIS['64bit']
-  end
-end
-
-describe Boxes do
-  let(:started_at) { Time.now }
-  let(:cluster) do
-    cluster = MiniTest::Mock.new
-    cluster.expect(:servers, [
-      id: "i-12345",
-      ip: "1.2.3.4",
-      type: 'c1.xlarge',
-      started_at: started_at,
-      tags: {'name' => 'pinky'}
-    ])
-    cluster
-  end
-
-  let(:boxes) { Boxes.new(cluster) }
-
-  it "updates from cluster" do
-    boxes.count.must_equal 0
-    boxes.update!
-    boxes.count.must_equal 1
-  end
-
-  it "has boxes" do
-    boxes.update!
-    box = boxes.first
-    box.id.must_equal "i-12345"
-    box.ip.must_equal "1.2.3.4"
-    box.type.id.must_equal "c1.xlarge"
-    box.started_at.must_equal started_at
-    box.tags.must_equal({'name' => 'pinky'})
-  end
-end
-
 describe Box do
   let(:started_at) { Time.now }
   let(:box_type) { BoxType.find('c1.xlarge') }
