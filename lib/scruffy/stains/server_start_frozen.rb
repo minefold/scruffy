2892fd293916af5c4d0d6c46f95ad466f5297478
Dave Newman <dave@snappyco.de>
2013-03-15 14:08:20 -0700

    don't log so much frozen

--- lib/scruffy/stains/server_start_frozen.rb
+++ lib/scruffy/stains/server_start_frozen.rb
@@ -12,23 +12,25 @@
 
 class ServerStartFrozen < Stain
   include StainWatcher
-  
+
   stain :server_start_frozen
-  
+
   def affected_ids
     @servers.starting.ids
   end
-  
+
   def check_stain(stain)
-    if stain.duration < 5 * 60
-      log.warn event: stain_type, 
-        server: stain.id,
-        duration: stain.duration
-    else
+    if stain.duration > 5 * 60
       server = @servers.up.find{|s| s.players.include?(stain.id) }
       log.warn event: 'server_start_frozen',
-        id: stain.id,
-        action: 'killing'
+        server: stain.id,
+        action: 'killing',
+        duration: stain.duration
+
+    if stain.duration > 1 * 60
+      log.info event: stain_type,
+        server: stain.id,
+        duration: stain.duration
     end
   end
 end
