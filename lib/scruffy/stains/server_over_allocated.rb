5b6ec53256cade95ededd4ffe81599710a75ac72
Dave Newman <dave@snappyco.de>
2013-04-28 20:24:50 -0700

    don't reallocate

--- lib/scruffy/stains/server_over_allocated.rb
+++ lib/scruffy/stains/server_over_allocated.rb
@@ -1,54 +1,54 @@
-# causes:
-#   not enough players in server
-
-# smell:
-#   less players in server than slots allocated
-
-# cleanup:
-#   reallocate server
-
-# report:
-
-class ServerOverAllocated < Stain
-  include StainWatcher
-
-  # TODO remove hardcoded reference to TF2
-  TF2 = '50bec3967aae5797c0000004'
-
-  stain :server_over_allocated
-
-  def affected_ids
-    @servers.select do |server|
-      lower_bound = ((server.slots || 1) - 1) * allocator.players_per_slot
-
-      (server.funpack != TF2) && (server.players.size < lower_bound)
-    end.ids
-  end
-
-  def stain_gone(stain_id)
-    log.info event: 'server_allocated_ok', id: stain_id
-  end
-
-  def check_stain(stain)
-    server = @servers.find_id(stain.id)
-    required = allocator.slots_required(server.players.size)
-    slot_difference = Math.log(server.slots, 2) - Math.log(required, 2)
-
-    if slot_difference > 2 && stain.duration > 30 * 60
-      log.warn event: 'server_over_allocated',
-        id: stain.id,
-        action: 'reallocating',
-        slots: server.slots,
-        required: required
-
-      # @servers.reallocate!(stain.id, required, "Optimizing server. Please reconnect in 30 seconds")
-
-    else
-      log.info event: 'server_over_allocated',
-        id: stain.id,
-        duration: stain.duration,
-        slots: server.slots,
-        required: required
-    end
-  end
-end
+# # causes:
+# #   not enough players in server
+#
+# # smell:
+# #   less players in server than slots allocated
+#
+# # cleanup:
+# #   reallocate server
+#
+# # report:
+#
+# class ServerOverAllocated < Stain
+#   include StainWatcher
+#
+#   # TODO remove hardcoded reference to TF2
+#   TF2 = '50bec3967aae5797c0000004'
+#
+#   stain :server_over_allocated
+#
+#   def affected_ids
+#     @servers.select do |server|
+#       lower_bound = ((server.slots || 1) - 1) * allocator.players_per_slot
+#
+#       (server.funpack != TF2) && (server.players.size < lower_bound)
+#     end.ids
+#   end
+#
+#   def stain_gone(stain_id)
+#     log.info event: 'server_allocated_ok', id: stain_id
+#   end
+#
+#   def check_stain(stain)
+#     server = @servers.find_id(stain.id)
+#     required = allocator.slots_required(server.players.size)
+#     slot_difference = Math.log(server.slots, 2) - Math.log(required, 2)
+#
+#     if slot_difference > 2 && stain.duration > 30 * 60
+#       log.warn event: 'server_over_allocated',
+#         id: stain.id,
+#         action: 'reallocating',
+#         slots: server.slots,
+#         required: required
+#
+#       # @servers.reallocate!(stain.id, required, "Optimizing server. Please reconnect in 30 seconds")
+#
+#     else
+#       log.info event: 'server_over_allocated',
+#         id: stain.id,
+#         duration: stain.duration,
+#         slots: server.slots,
+#         required: required
+#     end
+#   end
+# end
--- lib/scruffy/stains/server_under_allocated.rb
+++ lib/scruffy/stains/server_under_allocated.rb
@@ -1,58 +1,58 @@
-# causes:
-#   too many players in server
-
-# smell:
-#   more players in server than slots allocated
-
-# cleanup:
-#   reallocate server
-
-# report:
-
-class ServerUnderAllocated < Stain
-  include StainWatcher
-
-  stain :server_under_allocated
-  
-  # TODO remove hardcoded reference to TF2
-  TF2 = '50bec3967aae5797c0000004'
-
-  def affected_ids
-    @servers.select do |server|
-      player_slots_available =
-        (server.slots || 1) * allocator.players_per_slot
-
-      (server.funpack != TF2) && (server.players.size > player_slots_available)
-    end.ids
-  end
-
-  def stain_gone(stain_id)
-    log.info event: 'server_allocated_ok', id: stain_id
-  end
-
-  def check_stain(stain)
-    server = @servers.find_id(stain.id)
-    required = allocator.slots_required(server.players.size)
-    server_slots = (server.slots || 1)
-    
-    slot_difference = Math.log(required, 2) - Math.log(server_slots, 2)
-
-    if stain.duration > 10 * 60 or slot_difference > 1
-      log.warn event: 'server_under_allocated',
-        id: stain.id,
-        action: 'reallocating',
-        slots: server_slots,
-        required: required
-
-      @servers.reallocate!(stain.id, required, "Optimizing server. Please reconnect in 30 seconds")
-      forget stain.id
-
-    else
-      log.info event: 'server_under_allocated',
-        id: stain.id,
-        duration: stain.duration,
-        slots: server_slots,
-        required: required
-    end
-  end
-end
+# # causes:
+# #   too many players in server
+#
+# # smell:
+# #   more players in server than slots allocated
+#
+# # cleanup:
+# #   reallocate server
+#
+# # report:
+#
+# class ServerUnderAllocated < Stain
+#   include StainWatcher
+#
+#   stain :server_under_allocated
+#
+#   # TODO remove hardcoded reference to TF2
+#   TF2 = '50bec3967aae5797c0000004'
+#
+#   def affected_ids
+#     @servers.select do |server|
+#       player_slots_available =
+#         (server.slots || 1) * allocator.players_per_slot
+#
+#       (server.funpack != TF2) && (server.players.size > player_slots_available)
+#     end.ids
+#   end
+#
+#   def stain_gone(stain_id)
+#     log.info event: 'server_allocated_ok', id: stain_id
+#   end
+#
+#   def check_stain(stain)
+#     server = @servers.find_id(stain.id)
+#     required = allocator.slots_required(server.players.size)
+#     server_slots = (server.slots || 1)
+#
+#     slot_difference = Math.log(required, 2) - Math.log(server_slots, 2)
+#
+#     if stain.duration > 10 * 60 or slot_difference > 1
+#       log.warn event: 'server_under_allocated',
+#         id: stain.id,
+#         action: 'reallocating',
+#         slots: server_slots,
+#         required: required
+#
+#       @servers.reallocate!(stain.id, required, "Optimizing server. Please reconnect in 30 seconds")
+#       forget stain.id
+#
+#     else
+#       log.info event: 'server_under_allocated',
+#         id: stain.id,
+#         duration: stain.duration,
+#         slots: server_slots,
+#         required: required
+#     end
+#   end
+# end
